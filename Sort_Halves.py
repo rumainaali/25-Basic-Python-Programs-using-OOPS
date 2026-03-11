153b713a80fb609dce7cf52ebbaf9417e5de19bf
Rumaina.A <126662274+rumainaali@users.noreply.github.com>
2026-03-11 10:56:15 +0530

    Sort_Halves.py

--- Sort_Halves.py
+++ Sort_Halves.py
@@ -1,4 +1,33 @@
-# Sort the first half in ascending order and other half in descending order
+# Sort the first half in ascending order and other half in descending order - Way 1
+class SortHalves:
+    def sortHalves(self, num):
+        mid = len(num) // 2
+        
+        # Sort the First half
+        for i in range(mid):
+            for j in range(i+1,mid):
+                if num[i] > num[j]:
+                    num[i], num[j] = num[j], num[i]
+        
+        # Sort the Second half
+        for i in range(mid,len(num)):
+            for j in range(i+1, len(num)):
+                if num[i] < num[j]:
+                    num[i], num[j] = num[j], num[i]
+        return num
+
+obj = SortHalves()
+num = list(map(int,input("Enter the numbers in the list:").split()))
+result = obj.sortHalves(num)
+print("Sorted the first half in ascending order and other half in descending order:", result)
+"""
+Input: Enter the elements in the list: 8 7 6 5 4 3 2 1
+Output: Sorted the first half in ascending order and other half in descending order: [5,6,7,8,4,3,2,1]
+"""
+
+
+
+# Sort the first half in ascending order and other half in descending order - Way 2
 class SortHalves:
     def sortHalves(self, num):
         mid = len(num) // 2
@@ -14,6 +43,6 @@ result = obj.sortHalves(num)
 print("Sorted the first half in ascending order and other half in descending order:", result)
 
 """
-Input: Enter the elements in the list: 3 2 1 4 5 6
-Output: Sorted the first half in ascending order and other half in descending order: [1,2,3,6,5,4]
+Input: Enter the elements in the list: 5 4 3 2 1
+Output: Sorted the first half in ascending order and other half in descending order: [4,5,3,2,1]
 """
